d673a79fbdf76762c2931b97997a66e974e6cff9
miguelanguai <miguelangelguaivi@gmail.com>
2025-03-07 18:57:07 +0100

    feat: añadir subred

--- adma/ec2_01/main.tf
+++ adma/ec2_01/main.tf
@@ -18,6 +18,12 @@ resource "aws_vpc" "name" {
   }
 }
 
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.mi_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0e2c8caa4b6378d8c"
   instance_type = "t3.micro"
