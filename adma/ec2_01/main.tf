8c5e324786c28c6412a28f42341d2adddff2ad8c
miguelanguai <miguelangelguaivi@gmail.com>
2025-03-07 19:20:39 +0100

    feat: nombrar ec2 y añadir referencia a subred y grupo de seguridad para ec2

--- adma/ec2_01/main.tf
+++ adma/ec2_01/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "mi_vpc" {
   }
 }
 
-resource "aws_subnet" "name" {
+resource "aws_subnet" "misubnet" {
   vpc_id = aws_vpc.mi_vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "us-east-1a"
@@ -36,9 +36,11 @@ resource "aws_security_group" "gs_migrupo" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "miec2" {
   ami = "ami-0e2c8caa4b6378d8c"
   instance_type = "t3.micro"
+  subnet_id = aws_subnet.misubnet.id
+  vpc_security_group_ids = [aws_security_group.gs_migrupo.id]
   key_name = "vockey"
   user_data = <<-EOF
       #!/bin/bash
