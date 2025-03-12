7bd5ef69e208c989155a11e9e02f1a2f44b14b9e
miguelanguai <miguelangelguaivi@gmail.com>
2025-03-12 17:57:47 +0100

    feat: crear archivo terraform para azure

---
+++ azure.tf
@@ -0,0 +1,74 @@
+#ejecutar az login y loguearse desde la carpeta
+#puede ser que la primera vez terraform plan no funcione hay que esperar un poco
+provider "azurerm" {
+  features {}
+  subscription_id = "aad46404-f6ce-4ca6-b5d1-6157ac1030cd"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "mi-grupo-recursos"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "mi-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "mi-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "mi-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"  # <-- Cambiar a Static
+  sku                 = "Standard"
+}
+
+
+resource "azurerm_network_interface" "nic" {
+  name                = "mi-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "mi-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "mi-vm-linux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # 1 vCPU, 1 GB RAM
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
